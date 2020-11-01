d43c6ec99192deace39e3f4e6425fe6f17be01a5
evelandy <evelandy@gmail.com>
2020-11-01 08:28:03 -0600

    various test fixes

--- components/controls/schedule/TempTriggerTime.js
+++ components/controls/schedule/TempTriggerTime.js
@@ -31,6 +31,7 @@ export default class TempTriggerTime extends React.Component {
             [key]: val
         });
     }
+    
 
     async setTempTriggerTime() {
         let token = await AsyncStorage.getItem('x-access-token');
